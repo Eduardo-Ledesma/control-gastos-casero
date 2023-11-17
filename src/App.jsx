2c6307d1feba8b1a969d7fe5eb3acafe3cb1d6ea
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2023-11-17 15:13:17 -0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,6 @@ function App() {
     
       !username || !access ? (
         <ChooseUser />
-      ) : apiDown ? (
-        <div className='flex flex-col items-center gap-10 mt-10 text-3xl text-gray-200 p-6'>
-          <p>Se cayó again D:</p>
-        </div>
       ) : (
         <>
           <header>
@@ -61,7 +57,11 @@ function App() {
                 </svg>
               </button>
             </div>
-            { expenses.length ? (
+            { apiDown ? (
+              <div className='flex flex-col items-center gap-10 mt-10 text-3xl text-gray-200 p-6'>
+                <p>Se cayó again D:</p>
+              </div>
+            ) : expenses.length ? (
               <article className='mt-6 md:mt-0 px-4'>
                   <h2 className='text-center text-4xl text-green-400 mb-14 font-bold'>Gastos Registrados:</h2>
                   <section className='flex flex-col gap-y-10 xl:flex-row lg:gap-x-4 xl:px-10 justify-around'>
